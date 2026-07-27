271667eff24542222698371e9bf47bada7b00776
Musku Akshay <muskuakshay@Muskus-MacBook-Air.local>
2026-07-27 18:00:42 -0400

    1C-repository-layer

---
+++ cart-service/src/main/java/com/enterprise/cartservice/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.enterprise.cartservice.repository;
+
+import com.enterprise.cartservice.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+}
